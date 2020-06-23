cf17ac72614859d20a640fc67dd432c9cdc7f5cb
Samuel Marques <samuel95marques+github@gmail.com>
2020-06-23 19:05:50 +0100

    Added Entities and .gitignore

---
+++ ShokuDex/Data/Base/DescribedEntity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Recodme.ShokuDex.Data.Base
+{
+    public class DescribedEntity : NamedEntity
+    {
+        private string _description;
+        [Required]
+        public string Description
+        {
+            get => _description;
+            set
+            {
+                _description = value;
+                RegisterChange();
+            }
+        }
+
+        public DescribedEntity(string name, string description) : base(name)
+        {
+            _description = description;
+        }
+
+        public DescribedEntity(Guid id, DateTime createdAt, DateTime updatedAd, bool isDeleted, string name, string description) : base(id, createdAt, updatedAd, isDeleted, name)
+        {
+            _description = description;
+        }
+    }
+}
---
+++ ShokuDex/Data/Base/Entity.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Recodme.ShokuDex.Data.Base
+{
+    public abstract class Entity
+    {
+        [Key]
+        public Guid Id { get; private set; }
+
+        [Display(Name = "Created At")]
+        [Required]
+        public DateTime CreatedAt { get; private set; }
+
+        [Display(Name = "Updated At")]
+        [Required]
+        public DateTime UpdatedAt { get; protected set; }
+
+        private bool _isDeleted;
+        [Display(Name = "Deleted")]
+        [Required]
+        public bool IsDeleted
+        {
+            get => _isDeleted;
+            set
+            {
+                _isDeleted = value;
+                RegisterChange();
+            }
+        }
+
+        protected virtual void RegisterChange()
+        {
+            UpdatedAt = DateTime.UtcNow;
+        }
+
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+            CreatedAt = DateTime.UtcNow;
+            UpdatedAt = CreatedAt;
+            _isDeleted = false;
+        }
+
+        protected Entity(Guid id, DateTime createdAt, DateTime updatedAd, bool isDeleted)
+        {
+            Id = id;
+            CreatedAt = createdAt;
+            UpdatedAt = updatedAd;
+            _isDeleted = isDeleted;
+        }
+    }
+}
