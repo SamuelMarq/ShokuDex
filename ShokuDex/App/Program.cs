a984a23f1c60ac29faf49c77c97088dd648dd8ae
cmevrgarcia <c.garcia@recodme.pt>
2020-06-26 09:48:50 +0100

    Created namespaces

---
+++ ShokuDex/App/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Recodme.ShokuDex.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var ctx = new FoodLogContext();
+            //ctx.DataBase.EnsureCreated();
+
+
+        }
+    }
+}
