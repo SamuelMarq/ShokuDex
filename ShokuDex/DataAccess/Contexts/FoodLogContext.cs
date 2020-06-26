99b0ff5d05e06ae55afcbe4e56e2bbcf73adaa8d
cmevrgarcia <c.garcia@recodme.pt>
2020-06-26 12:25:51 +0100

    Added FoodLogContext

---
+++ ShokuDex/DataAccess/Contexts/FoodLogContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Recodme.ShokuDex.Data.FoodInfo;
+using Recodme.ShokuDex.Data.UserInfo;
+using Recodme.ShokuDex.DataAccess.Properties;
+
+namespace Recodme.ShokuDex.DataAccess.Contexts
+{
+    public class FoodLogContext : IdentityDbContext
+    {
+        public FoodLogContext() : base()
+        {
+
+        }
+
+        public FoodLogContext(DbContextOptions<FoodLogContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Resources.ConnectionString);
+            }
+        }
+
+        //protected override void OnModelCreating(ModelBuilder builder)
+        //{
+        //    builder.Entity<Profile>().HasOne(x => x.User).WithOne(x => x.Profile);
+        //    base.OnModelCreating(builder);
+        //}
+
+        public DbSet<Categories> Categories { get; set; }
+        public DbSet<Foods> Foods { get; set; }
+        public DbSet<Profiles> Profiles { get; set; }
+    }
+
+}
