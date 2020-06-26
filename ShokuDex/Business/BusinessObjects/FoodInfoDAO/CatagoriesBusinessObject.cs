6a6ad389c816ace0cb24cf71080b0ef6d12c5f25
Samuel Marques <samuel95marques+github@gmail.com>
2020-06-26 12:39:58 +0100

    Added CatagoriesBO

---
+++ ShokuDex/Business/BusinessObjects/FoodInfoDAO/CatagoriesBusinessObject.cs
@@ -0,0 +1,218 @@
+﻿using Recodme.ShokuDex.Data.FoodInfo;
+using Recodme.ShokuDex.DataAccess.DataAccessObjects;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Recodme.ShokuDex.Business.BusinessObjects.FoodInfoDAO
+{
+    public class CatagoriesBusinessObject
+    {
+        private BaseDataAccessObject<Categories> _dao;
+        public CatagoriesBusinessObject()
+        {
+            _dao = new BaseDataAccessObject<Categories>();
+        }
+
+        private TransactionOptions opts = new TransactionOptions()
+        {
+            IsolationLevel = IsolationLevel.ReadCommitted,
+            Timeout = TimeSpan.FromSeconds(30)
+        };
+
+      #region Create
+        public OperationResult Create(Categories item)
+        {
+            try
+            {
+                var scope = new TransactionScope(TransactionScopeOption.Required, opts, TransactionScopeAsyncFlowOption.Enabled);
+                _dao.Create(item);
+                scope.Complete();
+                return new OperationResult() { Success = true };
+            }
+            catch (Exception e)
+            {
+                return new OperationResult() { Success = false, Exception = e };
+            }
+        }
+
+        public async Task<OperationResult> CreateAsync(Categories item)
+        {
+            try
+            {
+                var scope = new TransactionScope(TransactionScopeOption.Required, opts, TransactionScopeAsyncFlowOption.Enabled);
+                await _dao.CreateAsync(item);
+                scope.Complete();
+                return new OperationResult() { Success = true };
+            }
+            catch (Exception e)
+            {
+                return new OperationResult() { Success = false, Exception = e };
+            }
+        }
+        #endregion
+
+        #region Read
+        public OperationResult<Categories> Read(Guid id)
+        {
+            try
+            {
+                var scope = new TransactionScope(TransactionScopeOption.Required, opts, TransactionScopeAsyncFlowOption.Enabled);
+                var res = _dao.Read(id);
+                transactionScope.Complete();
+                return new OperationResult<Categories>() { Success = true, Result = res };
+            }
+            catch (Exception e)
+            {
+                return new OperationResult<Categories>() { Success = false, Exception = e };
+            }
+        }
+
+        public async Task<OperationResult<Categories>> ReadAsync(Guid id)
+        {
+            try
+            {
+                var scope = new TransactionScope(TransactionScopeOption.Required, opts, TransactionScopeAsyncFlowOption.Enabled);
+                var res = await _dao.ReadAsync(id);
+                transactionScope.Complete();
+                return new OperationResult<Categories>() { Success = true, Result = res };
+            }
+            catch (Exception e)
+            {
+                return new OperationResult<Categories>() { Success = false, Exception = e };
+            }
+        }
+        #endregion
+
+        #region Update
+        public OperationResult Update(Categories item)
+        {
+            try
+            {
+                var scope = new TransactionScope(TransactionScopeOption.Required, opts, TransactionScopeAsyncFlowOption.Enabled);
+                _dao.Update(item);
+                transactionScope.Complete();
+                return new OperationResult() { Success = true };
+            }
+            catch (Exception e)
+            {
+                return new OperationResult() { Success = false, Exception = e };
+            }
+
+        }
+
+        public async Task<OperationResult> UpdateAsync(Categories item)
+        {
+            try
+            {
+                var scope = new TransactionScope(TransactionScopeOption.Required, opts, TransactionScopeAsyncFlowOption.Enabled);
+                await _dao.UpdateAsync(item);
+                scope.Complete();
+                return new OperationResult() { Success = true };
+            }
+            catch (Exception e)
+            {
+                return new OperationResult() { Success = false, Exception = e };
+            }
+        }
+        #endregion
+
+        #region Delete
+        public OperationResult Delete(Categories item)
+        {
+            try
+            {
+                var scope = new TransactionScope(TransactionScopeOption.Required, opts, TransactionScopeAsyncFlowOption.Enabled);
+                _dao.Delete(item);
+                transactionScope.Complete();
+                return new OperationResult() { Success = true };
+            }
+            catch (Exception e)
+            {
+                return new OperationResult() { Success = false, Exception = e };
+            }
+
+        }
+
+        public async Task<OperationResult> DeleteAsync(Categories item)
+        {
+            try
+            {
+                var scope = new TransactionScope(TransactionScopeOption.Required, opts, TransactionScopeAsyncFlowOption.Enabled);
+                await _dao.DeleteAsync(item);
+                scope.Complete();
+                return new OperationResult() { Success = true };
+            }
+            catch (Exception e)
+            {
+                return new OperationResult() { Success = false, Exception = e };
+            }
+        }
+
+
+        public OperationResult Delete(Guid id)
+        {
+            try
+            {
+                var scope = new TransactionScope(TransactionScopeOption.Required, opts, TransactionScopeAsyncFlowOption.Enabled);
+                _dao.Delete(id);
+                transactionScope.Complete();
+                return new OperationResult() { Success = true };
+            }
+            catch (Exception e)
+            {
+                return new OperationResult() { Success = false, Exception = e };
+            }
+
+        }
+
+        public async Task<OperationResult> DeleteAsync(Guid id)
+        {
+            try
+            {
+                var scope = new TransactionScope(TransactionScopeOption.Required, opts, TransactionScopeAsyncFlowOption.Enabled);
+                await _dao.DeleteAsync(id);
+                scope.Complete();
+                return new OperationResult() { Success = true };
+            }
+            catch (Exception e)
+            {
+                return new OperationResult() { Success = false, Exception = e };
+            }
+        }
+        #endregion
+
+        #region List
+        public OperationResult<List<Categories>> List()
+        {
+            try
+            {
+                var scope = new TransactionScope(TransactionScopeOption.Required, opts, TransactionScopeAsyncFlowOption.Enabled);
+                var res = _dao.List();
+                transactionScope.Complete();
+                return new OperationResult<List<Categories>>() { Success = true, Result = res };
+            }
+            catch (Exception e)
+            {
+                return new OperationResult<List<Categories>>() { Success = false, Exception = e };
+            }
+        }
+
+        public async Task<OperationResult<List<Categories>>> ListAsync()
+        {
+            try
+            {
+                var scope = new TransactionScope(TransactionScopeOption.Required, opts, TransactionScopeAsyncFlowOption.Enabled);
+                var res = await _dao.ListAsync();
+                transactionScope.Complete();
+                return new OperationResult<List<Categories>>() { Success = true, Result = res };
+            }
+            catch (Exception e)
+            {
+                return new OperationResult<List<Categories>>() { Success = false, Exception = e };
+            }
+        }
+        #endregion
+    }
+}
