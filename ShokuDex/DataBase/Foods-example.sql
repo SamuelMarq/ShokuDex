f85ffab3241530d1fe46a9f8a3cd9626115f9da2
Samuel Marques <samuel95marques+github@gmail.com>
2020-07-09 15:42:41 +0100

    Added SQL inserts

---
+++ ShokuDex/DataBase/Foods-example.sql
@@ -0,0 +1,11 @@
+USE ShokuDex
+
+INSERT INTO Foods
+           ([CreatedAt],[UpdatedAt],[IsDeleted],[Name],[Description],[Fats],[Carbohydrates],[Protein],[Alcohol],[Calories],[Portion],[Photo],[IsRecipe],[ProfileId],[CategoryId]) VALUES
+           (GETDATE(), GETDATE(), 0, 'Manhã', '', 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, '' , 0, '00000000-0000-0000-0000-000000000001', 'id da categoria'),
+           (GETDATE(), GETDATE(), 0, 'Manhã', '', 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, '' , 0, '00000000-0000-0000-0000-000000000001', 'id da categoria'),
+           (GETDATE(), GETDATE(), 0, 'Manhã', '', 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, '' , 0, '00000000-0000-0000-0000-000000000001', 'id da categoria'),
+           (GETDATE(), GETDATE(), 0, 'Manhã', '', 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, '' , 0, '00000000-0000-0000-0000-000000000001', 'id da categoria'),
+           (GETDATE(), GETDATE(), 0, 'Manhã', '', 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, '' , 0, '00000000-0000-0000-0000-000000000001', 'id da categoria');
+
+
