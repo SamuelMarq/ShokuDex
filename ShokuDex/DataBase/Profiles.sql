eb0d704f7f67695551f5dfb9da3719168be5e905
cmevrgarcia <c.garcia@recodme.pt>
2020-07-09 15:55:59 +0100

    Added Profiles.sql

---
+++ ShokuDex/DataBase/Profiles.sql
@@ -0,0 +1,4 @@
+use ShokuDex
+
+insert into Profiles(Id, CreatedAt, UpdatedAt, IsDeleted, Name, Description) values
+('00000000-0000-0000-0000-000000000001', GETDATE(), GETDATE(), 0, 'Admin', 'Administrator')
