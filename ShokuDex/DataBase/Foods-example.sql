90c0c050062520566b8bd8d94c0b705a13872945
SamuelMarq <samuel95marques+github@gmail.com>
2020-07-13 16:24:43 +0100

    Added more Foods to Foods.sql

--- ShokuDex/DataBase/Foods-example.sql
+++ ShokuDex/DataBase/Foods-example.sql
@@ -1,9 +1,9 @@
 USE ShokuDex
 
 INSERT INTO Foods
-           ([CreatedAt],[UpdatedAt],[IsDeleted],[Name],[Description],[Fats],[Carbohydrates],[Protein],[Alcohol],[Calories],[Portion],[Photo],[IsRecipe],[ProfileId],[CategoryId]) VALUES
-           (GETDATE(), GETDATE(), 0, 'Manhã', '', 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, '' , 0, '00000000-0000-0000-0000-000000000001', 'id da categoria'),
-           (GETDATE(), GETDATE(), 0, 'Manhã', '', 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, '' , 0, '00000000-0000-0000-0000-000000000001', 'id da categoria'),
-           (GETDATE(), GETDATE(), 0, 'Manhã', '', 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, '' , 0, '00000000-0000-0000-0000-000000000001', 'id da categoria'),
-           (GETDATE(), GETDATE(), 0, 'Manhã', '', 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, '' , 0, '00000000-0000-0000-0000-000000000001', 'id da categoria'),
-           (GETDATE(), GETDATE(), 0, 'Manhã', '', 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, '' , 0, '00000000-0000-0000-0000-000000000001', 'id da categoria');
+           ([Id],[CreatedAt],[UpdatedAt],[IsDeleted],[Name],[Description],[Fats],[Carbohydrates],[Protein],[Alcohol],[Calories],[Portion],[Photo],[IsRecipe],[ProfileId],[CategoryId]) VALUES
+           (NEWID(), GETDATE(), GETDATE(), 0, 'NomeDaComida', '', 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, '' , 0, '00000000-0000-0000-0000-000000000001', 'id da categoria'),
+           (NEWID(), GETDATE(), GETDATE(), 0, 'NomeDaComida', '', 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, '' , 0, '00000000-0000-0000-0000-000000000001', 'id da categoria'),
+           (NEWID(), GETDATE(), GETDATE(), 0, 'NomeDaComida', '', 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, '' , 0, '00000000-0000-0000-0000-000000000001', 'id da categoria'),
+           (NEWID(), GETDATE(), GETDATE(), 0, 'NomeDaComida', '', 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, '' , 0, '00000000-0000-0000-0000-000000000001', 'id da categoria'),
+           (NEWID(), GETDATE(), GETDATE(), 0, 'NomeDaComida', '', 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, '' , 0, '00000000-0000-0000-0000-000000000001', 'id da categoria');
