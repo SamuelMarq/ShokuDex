7d9d0c56ff15edcd63d8d6b73b94d66b9efee1ee
rafael pereira <rafael_pereira12@outlook.com>
2020-07-21 15:45:27 +0100

    Updated _Layout

--- ShokuDex/WebApi/wwwroot/js/site.js
+++ ShokuDex/WebApi/wwwroot/js/site.js
@@ -1,4 +1,31 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
 
-// Write your JavaScript code.
+$(document).ready(function () {
+
+
+    $('#sidebarCollapse').on('click', function () {
+        
+        $('#sidebar').toggleClass('active');
+
+        $('.collapse.in').toggleClass('in');
+
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+
+});
+
+
+
+$(document).ready(function () {
+
+
+    $('#sidebarCollapse').on('click', function () {
+
+        $('#content').toggleClass('active');
+
+        $('.collapse.in').toggleClass('in');
+
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+
+});
