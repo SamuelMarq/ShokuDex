1a81e15f3c285d0294275890a59ee0f549716dbd
SamuelMarq <samuel95marques+github@gmail.com>
2020-07-23 16:14:38 +0100

    Added Administrator Options page

---
+++ ShokuDex/WebApi/Controllers/Web/Other/AdministratorOptionsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Recodme.ShokuDex.WebApi.Controllers.Web.Other
+{
+    public class AdministratorOptionsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
